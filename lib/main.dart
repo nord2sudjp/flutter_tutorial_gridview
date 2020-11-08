299db5f397c1ee6740649cfbe5c92d8d7b16dd10
maka y <nord2sudjp@gmail.com>
2020-11-08 17:33:43 +0900

    state and button

---
+++ lib/main.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Stateful',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Stateful'),
+        ),
+        body: Center(
+          //child: ClickGood(),
+          child: ChangeForm()
+        ),
+      ),
+    );
+  }
+}
+
+class ChangeForm extends StatefulWidget {
+  @override
+  _ChangeFormState createState() => _ChangeFormState();
+}
+
+class _ChangeFormState extends State<ChangeForm> {
+  int _count = 0;
+
+  void _handlePressed() {
+    setState(() { //クリックするとカウントアップ
+      _count++;
+    });
+  }
+
+  Widget build(BuildContext context) {
+    return Container(
+        padding: const EdgeInsets.all(50.0),
+        child: Column(
+          children: <Widget>[
+            Text(
+              "$_count", //これでcountを表示する
+              style: TextStyle(
+                  color:Colors.blueAccent,
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.w500
+              ),
+            ),
+            FlatButton(
+              onPressed: _handlePressed, // 押すと_handlePressedを呼び出す
+              color: Colors.blue,
+              child: Text(
+                '更新',
+                style: TextStyle(
+                    color:Colors.white,
+                    fontSize: 20.0
+                ),
+              ),
+            )
+          ],
+        )
+    );
+  }
+}
+
+class ClickGood extends StatefulWidget {
+  @override
+  _ClickGoodState createState() => _ClickGoodState();
+}
+
+class _ClickGoodState extends State<ClickGood> {
+  bool _active = false;
+
+  void _handleTap() {
+    setState(() {
+      _active = !_active;
+    });
+  }
+
+  Widget build(BuildContext context) {
+    return GestureDetector(
+        onTap: _handleTap,
+        child: Container(
+          child: Column(
+              children: <Widget>[
+                Container(
+                  child: Center(
+                    child: new Icon(
+                      Icons.thumb_up,
+                      color: _active ? Colors.orange[700] : Colors.grey[500],
+                      size: 100.0,
+                    ),
+                  ),
+                  width: 200.0,
+                  height: 200.0,
+                ),
+                Container(
+                  child: Center(
+                    child: Text(
+                      _active ? 'Active' : 'Inactive',
+                      style: TextStyle(fontSize: 32.0, color: Colors.white),
+                    ),
+                  ),
+                  width: 200.0,
+                  height: 50.0,
+                  decoration: BoxDecoration(
+                    color: _active ? Colors.orange[700] : Colors.grey[600],
+                  ),
+                ),
+              ]
+          ),
+        )
+    );
+  }
+}
