ac0010e3e5e8267e6721ca4f9f79a1ff214f7882
maka y <nord2sudjp@gmail.com>
2020-11-08 17:57:52 +0900

    Text Form

--- lib/main.dart
+++ lib/main.dart
@@ -16,12 +16,60 @@ class MyApp extends StatelessWidget {
         body: Center(
           //child: ClickGood(),
           //child: ChangeForm(),
-          child: DropDown(),
+          //child: DropDown(),
+          child: TextForm(),
         ),
       ),
     );
   }
 }
+
+class TextForm extends StatefulWidget {
+  @override
+  _TextFormState createState() => _TextFormState();
+}
+
+class _TextFormState extends State<TextForm> {
+
+  String _text = '';
+
+  void _handleText(String e) {
+    setState(() {
+      _text = e;
+    });
+  }
+
+  Widget build(BuildContext context) {
+    return Container(
+        padding: const EdgeInsets.all(50.0),
+        child: Column(
+          children: <Widget>[
+            Text(
+              "$_text",
+              style: TextStyle(
+                  color: Colors.blueAccent,
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.w500
+              ),
+            ),
+            new TextField(
+              enabled: true,
+              // 入力数
+              maxLength: 10,
+              maxLengthEnforced: false,
+              style: TextStyle(color: Colors.red),
+              obscureText: false,
+              maxLines:1 ,
+              //パスワード
+              onChanged: _handleText,
+            ),
+          ],
+        )
+    );
+  }
+}
+
+
 
 class DropDown extends StatefulWidget {
   @override
